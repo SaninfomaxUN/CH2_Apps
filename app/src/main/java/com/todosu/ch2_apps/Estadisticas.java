555b9cea6b20bbb15c68afa113e2c3b472bc7b47
S4N <dcruzhe@unal.edu.co>
2023-10-29 12:05:42 -0500

    Pantalla Estadisticas + Fonts + MPAndroidChart Library Added

---
+++ app/src/main/java/com/todosu/ch2_apps/Estadisticas.java
@@ -0,0 +1,64 @@
+package com.todosu.ch2_apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class Estadisticas extends AppCompatActivity {
+    private HorizontalBarChart horizontalBarChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas);
+
+        horizontalBarChart = findViewById(R.id.horizontalBarChart);
+
+
+        BarData data = new BarData(getDataSet());
+        horizontalBarChart.setData(data);
+        horizontalBarChart.animateXY(2000, 2000);
+        horizontalBarChart.invalidate();
+    }
+
+    private BarDataSet getDataSet() {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries,"hi");
+        dataset.setColor(Color.parseColor("#3f8d55"));
+        return dataset;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        return labels;
+    }
+
+
+
+
+}
