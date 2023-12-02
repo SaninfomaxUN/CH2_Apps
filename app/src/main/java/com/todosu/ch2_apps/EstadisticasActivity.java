1b5223483b76c32f2db18f8d238f6608dd055080
S4N <dcruzhe@unal.edu.co>
2023-12-02 00:29:30 -0500

    Dialogo de Eventos de Prueba

--- app/src/main/java/com/todosu/ch2_apps/EstadisticasActivity.java
+++ app/src/main/java/com/todosu/ch2_apps/EstadisticasActivity.java
@@ -1,21 +1,28 @@
 package com.todosu.ch2_apps;
 
+import android.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.todosu.ch2_apps.models.Evento;
 
 import java.util.ArrayList;
 
 public class EstadisticasActivity extends AppCompatActivity {
     private HorizontalBarChart horizontalBarChart;
+
+    private Evento eventoSeleccionado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +34,39 @@ public class EstadisticasActivity extends AppCompatActivity {
         horizontalBarChart.animateXY(2000, 2000);
         horizontalBarChart.invalidate();
 
+        seleccionarEventos();
         regresar();
+    }
+
+    public void seleccionarEventos() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+       // Convertir ArrayList a CharSequence[]
+        /* CharSequence[] nombresCharSequence = Data.getListaEventosDelUsuarioConectado().stream()
+                .map(Evento::getNombreEvento)
+                .toArray(CharSequence[]::new);*/
+
+        final CharSequence[] eventos = new CharSequence[4];
+
+        eventos[0] = "Evento 1";
+        eventos[1] = "Evento 2";
+        eventos[2] = "Evento 3";
+        eventos[3] = "Evento 4";
+
+
+        builder.setTitle("Selecciona el evento:")
+                .setItems(eventos, (dialog, posEventoSeleccionado) -> {
+                    Toast.makeText(
+                                    EstadisticasActivity.this,
+                                    "Seleccionaste: " + eventos[posEventoSeleccionado],
+                                    Toast.LENGTH_SHORT)
+                            .show();
+                    eventoSeleccionado = Data.getEventoDelUsuarioConectado(eventos[posEventoSeleccionado].toString());
+                })
+                .setCancelable(false);
+
+        builder.create();
+        builder.show();
     }
 
     private BarDataSet getDataSet() {
