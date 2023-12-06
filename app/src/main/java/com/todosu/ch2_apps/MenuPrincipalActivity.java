61a9cc007665af7023c88696858b57a126d9d929
S4N <dcruzhe@unal.edu.co>
2023-12-05 22:10:05 -0500

    MenuPrincipal - Cerrar Sesión

--- app/src/main/java/com/todosu/ch2_apps/MenuPrincipalActivity.java
+++ app/src/main/java/com/todosu/ch2_apps/MenuPrincipalActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
@@ -15,34 +18,49 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu_principal);
 
         ImageView imageView5 = findViewById(R.id.imageView5);
-        imageView5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent imageView5 = new Intent(MenuPrincipalActivity.this, ConsejosActivity.class);
-                startActivity(imageView5);
+        imageView5.setOnClickListener(view -> {
+            Intent imageView51 = new Intent(MenuPrincipalActivity.this, ConsejosActivity.class);
+            startActivity(imageView51);
 
-            }
         });
 
         ImageView imageView6 = findViewById(R.id.imageView6);
-        imageView6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent imageView6 = new Intent(MenuPrincipalActivity.this, CalculadoraActivity.class);
-                startActivity(imageView6);
-            }
+        imageView6.setOnClickListener(view -> {
+            Intent imageView61 = new Intent(MenuPrincipalActivity.this, CalculadoraActivity.class);
+            startActivity(imageView61);
         });
 
-        ImageView imageView4 = findViewById(R.id.imageView4);
-        imageView4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent imageView4 = new Intent(MenuPrincipalActivity.this, InicioSesionActivity.class);
-                startActivity(imageView4);
-            }
-        });
-
-
+        inicializarBtnCerrarSesion();
 
     }
+
+    private void inicializarBtnCerrarSesion() {
+        ImageView btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        btnCerrarSesion.setOnClickListener(view -> closeSession());
+    }
+
+    //Sobreescritura del método para cerrar sesión al presionar el botón de atrás
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            closeSession();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    // Cerrar sesión
+    public void closeSession() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setMessage("¿Deseas cerrar sesión?");
+        builder.setPositiveButton("Salir", (dialogInterface, i) -> {
+            UsuarioConectado.removeUsuarioConectadSegunEmailUsuario();
+            Intent intent = new Intent(this, InicioSesionActivity.class);
+            startActivity(intent);
+        });
+        builder.setNegativeButton("Cancelar", null);
+        builder.show();
+    }
+
+
 }
