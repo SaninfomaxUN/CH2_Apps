8c512954ada090844f4e659415be209fd032b9fc
S4N <dcruzhe@unal.edu.co>
2023-12-05 21:32:01 -0500

    Login 100% Funcional ✅

--- app/src/main/java/com/todosu/ch2_apps/InicioSesionActivity.java
+++ app/src/main/java/com/todosu/ch2_apps/InicioSesionActivity.java
@@ -5,28 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputLayout;
+import com.todosu.ch2_apps.models.Usuario;
 
 public class InicioSesionActivity extends AppCompatActivity {
 
+    private MaterialButton btnIniciarSesion;
+    private TextInputLayout editTextCorreo;
+    private TextInputLayout editTextPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent button = new Intent(InicioSesionActivity.this, MenuPrincipalActivity.class);
-                startActivity(button);
-            }
-        });
+        findReferences();
+        inicializarBtnIniciarSesion();
+        
 
-        ImageView imageView2 = findViewById(R.id.imageView2);
+                
+
+       /* ImageView imageView2 = findViewById(R.id.imageView2);
         imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,16 +46,99 @@ public class InicioSesionActivity extends AppCompatActivity {
                 startActivity(imageView3);
 
             }
-        });
+        });*/
 
         TextView textView3 = findViewById(R.id.textView3);
-        textView3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent textView3 = new Intent(InicioSesionActivity.this, RegistroActivity.class);
-                startActivity(textView3);
-            }
+        textView3.setOnClickListener(view -> {
+            Intent textView31 = new Intent(InicioSesionActivity.this, RegistroActivity.class);
+            startActivity(textView31);
         });
 
     }
+    private void findReferences() {
+        btnIniciarSesion = findViewById(R.id.button);
+        editTextCorreo = findViewById(R.id.editTextCorreo);
+        editTextPass = findViewById(R.id.editTextPass);
+    }
+
+    private void inicializarBtnIniciarSesion() {
+        btnIniciarSesion.setOnClickListener(view -> {
+            checkLogin();
+        });
+
+    }
+    private void checkLogin() {
+        cleanErrors();
+        if (!checkComplete()) {
+            return;
+        }
+        Usuario usuarioEncontrado = checkEmail();
+        if (usuarioEncontrado == null) {
+            return;
+        }
+        if (!checkPass(usuarioEncontrado)) {
+            return;
+        }
+        cleanErrors();
+        logIn(usuarioEncontrado);
+    }
+
+    private boolean checkComplete() {
+        boolean isComplete = true;
+        if (editTextCorreo.getEditText().getText().toString().isEmpty()) {
+            editTextCorreo.setError("Ingresa tu correo");
+            isComplete = false;
+        }
+        if (!editTextCorreo.getEditText().getText().toString().contains("@")) {
+            editTextCorreo.setError("Ingresa un correo válido");
+            editTextCorreo.getEditText().setText("");
+            isComplete = false;
+        }
+        if (editTextPass.getEditText().getText().toString().isEmpty()) {
+            editTextPass.setError("Ingresa tu contraseña");
+            isComplete = false;
+        }
+        return isComplete;
+    }
+
+    private Usuario checkEmail() {
+
+        Usuario usuarioEncontrado = Data.getUsuarioPorEmail(editTextCorreo.getEditText().getText().toString());
+        if (usuarioEncontrado == null) {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+            builder.setMessage("El correo ingresado no se encuentra registrado. Si no tienes una cuenta, puedes registrarte!");
+            builder.setPositiveButton("Aceptar", null);
+            builder.show();
+            editTextCorreo.getEditText().setText("");
+            editTextPass.getEditText().setText("");
+            return null;
+        }
+        return usuarioEncontrado;
+    }
+
+    private boolean checkPass(Usuario usuarioEncontrado) {
+        editTextPass.getEditText().setText("");
+        if (usuarioEncontrado.getContrasena().equals(editTextPass.getEditText().getText().toString())) {
+            editTextPass.setError(null);
+            return true;
+        } else {
+            editTextPass.setError("Contraseña Incorrecta");
+            return false;
+        }
+    }
+
+    private void cleanErrors() {
+        editTextCorreo.setError(null);
+        editTextPass.setError(null);
+    }
+
+    private void logIn(Usuario usuarioEncontrado) {
+        UsuarioConectado.setUsuarioConectadoSegunEmailUsuario(usuarioEncontrado.getEmail());
+        Toast.makeText(this, "Bienvenido " + usuarioEncontrado.getNombreCompleto(), Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MenuPrincipalActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
