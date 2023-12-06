1c3c508e6a73b608326dd0e72bdf510ae8e1e327
S4N <dcruzhe@unal.edu.co>
2023-12-06 17:21:39 -0500

    MenuPrincipal - Added NavigationBar ✅

--- app/src/main/java/com/todosu/ch2_apps/MenuPrincipalActivity.java
+++ app/src/main/java/com/todosu/ch2_apps/MenuPrincipalActivity.java
@@ -2,16 +2,18 @@ package com.todosu.ch2_apps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
+    private BottomNavigationView navBarMenuPrincipal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +26,9 @@ public class MenuPrincipalActivity extends AppCompatActivity {
 
         });
 
-        ImageView imageView6 = findViewById(R.id.imageView6);
-        imageView6.setOnClickListener(view -> {
-            Intent imageView61 = new Intent(MenuPrincipalActivity.this, CalculadoraActivity.class);
-            startActivity(imageView61);
-        });
-
         inicializarBtnCerrarSesion();
+        findComponents();
+        setNavBarMenuPrincipalActions();
 
     }
 
@@ -38,6 +36,29 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         ImageView btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
         btnCerrarSesion.setOnClickListener(view -> closeSession());
     }
+
+    private void findComponents() {
+        navBarMenuPrincipal = findViewById(R.id.navBarMenuPrincipal);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void setNavBarMenuPrincipalActions() {
+        navBarMenuPrincipal.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.MisEventos) {
+                Intent intent = new Intent(this, ConsejosActivity.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.RegistrarConsumo) {
+                Intent intent = new Intent(this, CalculadoraActivity.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.Estadisticas) {
+                Intent intent = new Intent(this, EstadisticasActivity.class);
+                startActivity(intent);
+            }
+            return false;
+        });
+
+    }
+
 
     //Sobreescritura del método para cerrar sesión al presionar el botón de atrás
     @Override
