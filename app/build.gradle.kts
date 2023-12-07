115f321f8c56b7e71cabf27fc56ce3c7626bf2f8
Santiago Hernandez <dcruzhe@unal.edu.co>
2023-12-07 09:34:54 -0500

    Fixed details & strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0") // Asegúrate de verificar la última versión en el repositorio de GitHub.
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0") // Asegúrate de verificar la última versión en el repositorio de GitHub.
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
